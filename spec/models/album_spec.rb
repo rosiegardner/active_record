2f66e0949d51cf32e62084ad69d2c270645028dd
Rosie Gardner <rosiegardner78@gmail.com>
2022-05-26 19:28:43 -0700

    add testing for have and belong to many

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -5,6 +5,7 @@ describe Album do
   it { should have_many(:songs) }
   it { should validate_presence_of :name }
   it { should validate_length_of(:name).is_at_most(100) }
+  it { should have_and_belong_to_many :artists }
 
   it("titleizes the name of an album") do
     album = Album.create({name: "giant steps", genre: "jazz"})
