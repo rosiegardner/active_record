29a117a5a840d77c9fd310fcc1e564133467da8c
Rosie Gardner <rosiegardner78@gmail.com>
2022-05-26 19:25:15 -0700

    fix type for artists join table

--- db/migrate/20220527022140_add_artists_and_join_table.rb
+++ db/migrate/20220527022140_add_artists_and_join_table.rb
@@ -5,7 +5,7 @@ class AddArtistsAndJoinTable < ActiveRecord::Migration[5.2]
       t.timestamps
     end
 
-    create_table :albums_artists, id: falso do |t|
+    create_table :albums_artists, id: false do |t|
       t.belongs_to :artist, index: true
       t.belongs_to :album, index: true
     end
